2d684045ff1e8583fa128c914791a66330d041b8
YeYoung Kim <yeahy027@naver.com>
2025-05-08 15:45:37 +0900

    20250508 심화과제

---
+++ src/main/java/d20250508/ch17/sec06/exam01/MapExample.java
@@ -0,0 +1,19 @@
+package d20250508.ch17.sec06.exam01;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class MapExample {
+    public static void main(String[] args) {
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("홍길동", 85));
+        studentList.add(new Student("홍길동", 92));
+        studentList.add(new Student("홍길동", 87));
+
+        studentList.stream()
+                .mapToInt(s -> s.getScore())
+                .forEach(score -> System.out.println(score));
+    }
+}
---
+++ src/main/java/d20250508/ch17/sec06/exam03/FlatMappingExample.java
@@ -0,0 +1,32 @@
+package d20250508.ch17.sec06.exam03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FlatMappingExample {
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<>();
+        list1.add("this is java");
+        list1.add("i am a best developer");
+
+        // 문장 스트림을 단어 스트림으로 변환
+        list1.stream()
+                .flatMap(data -> Arrays.stream(data.split(" ")))
+                .forEach(word -> System.out.println(word));
+        System.out.println();
+
+        List<String> list2 = Arrays.asList("10, 20, 30", "40, 50");
+        // 문자열 숫자 목록 스트림을 숫자 스트림으로 변환
+        list2.stream()
+                .flatMapToInt(data -> {
+                    String[] strArr = data.split(",");
+                    int[] intArr = new int[strArr.length];
+                    for (int i = 0; i < strArr.length; i++) {
+                        intArr[i] = Integer.parseInt(strArr[i].trim());
+                    }
+                    return Arrays.stream(intArr);
+                })
+                .forEach(data -> System.out.println(data));
+    }
+}
